88292bab3984b605fad751a34a9a12d426b1167e
Francesca Libertini <f.libertini@aktsrl.com>
2018-02-20 10:38:53 +0100

    Nuovo messaggio

--- Esempio/src/it/mio/Main.java
+++ Esempio/src/it/mio/Main.java
@@ -10,9 +10,7 @@ public class Main {
 		
 		listMessage.add("Buongiorno");
 		listMessage.add("Arrivederci");
-		listMessage.add("ciao");
-		listMessage.add("Addio");
-		listMessage.add("Nessun saluto");
+	
 		for(String m: listMessage) {
 			System.out.println(m);
 		}
